2548b2c4f59755077b4e42a8f6e2647fdb0a63ff
Vichakshana Arangala <vichakshana.arangala@gmail.ocm>
2022-10-13 23:53:12 +0100

    successfully implemented the L system

---
+++ Assets/Scripts/LSystemGenerator.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+public class LSystemGenerator : MonoBehaviour
+{
+    public Rule[] rules;
+    public string rootSentence;
+    [Range(0,10)]
+    public int iterationLimit = 1;
+
+    private void Update() {
+        Debug.Log(GenerateSentence()); 
+    }
+
+    public string GenerateSentence(string word =null){
+        if(word ==null)
+        {
+            word = rootSentence;
+        }
+        return GrowRecursive(word);
+    }
+
+    private string GrowRecursive(string word, int iterationIndex= 0) { 
+    if(iterationIndex>=iterationLimit){
+            return word;
+        }
+
+        StringBuilder newWord = new StringBuilder();
+
+        foreach(var c in word) {
+            newWord.Append(c);
+            ProcessRulesRecursively(newWord, c, iterationIndex);
+        }
+
+        return newWord.ToString();
+    }
+
+    private void ProcessRulesRecursively (StringBuilder newWord, char c, int iterationIndex) {
+        foreach (var rule in rules){
+            if (rule.letter == c.ToString()) { 
+            newWord.AppendLine(GrowRecursive(rule.GetResult(),iterationIndex+1));
+            }
+        }
+    }
+
+}
